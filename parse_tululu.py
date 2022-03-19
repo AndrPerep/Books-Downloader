8433a26a954e12acc523284f5e3359c240ba8551
AndrPerep <79010353+AndrPerep@users.noreply.github.com>
2022-03-19 17:34:18 +0300

    общие функции вынесены в отдельный файл

---
+++ parse_tululu.py
@@ -0,0 +1,79 @@
+import requests
+import os
+
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from urllib.parse import unquote
+from pathvalidate import sanitize_filename
+from pathlib import Path
+
+
+def check_for_redirect(response):
+    if response.status_code in (301, 302):
+        raise requests.HTTPError
+
+
+def get_soup(url):
+    response = requests.get(url, allow_redirects=False)
+    response.raise_for_status()
+    check_for_redirect(response)
+    return BeautifulSoup(response.text, 'lxml')
+
+
+def parse_book_page(soup, book_id):
+    title_selector = 'h1'
+    title_text = soup.select_one(title_selector).text
+    name, author = str(title_text).split('::')
+    stripped_name = name.strip()
+    stripped_author = author.strip()
+    book_filename = sanitize_filename(f'{book_id}. {stripped_name}.txt')
+
+    book = {
+        'Заголовок': stripped_name,
+        'Автор': stripped_author,
+        'book_filename': book_filename
+    }
+
+    genres_selector = 'span.d_book a'
+    genres_tag = soup.select(genres_selector)
+    book['Жанры'] = [genre.text for genre in genres_tag]
+
+    comments = soup.find_all('div', class_='texts')
+    book['Комментарии'] = [comment.find('span').text for comment in comments]
+
+    base_url = 'http://tululu.org'
+    img_selector = 'div.bookimage img'
+    img_tag = soup.select_one(img_selector)['src']
+    img_url = urljoin(base_url, img_tag)
+    book['img_url'] = img_url
+    book['img_filename'] = sanitize_filename(unquote(img_url.split('/')[-1]))
+
+    return book
+
+
+def download_text(url, filename, folder, book_id):
+    payload = {
+        'id': str(book_id)
+    }
+    response = requests.get(url, params=payload, allow_redirects=False)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    sanitazed_filename = sanitize_filename(filename)
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    filepath = os.path.join(folder, sanitazed_filename)
+
+    with open(filepath, 'w') as file:
+        file.write(response.text)
+
+
+def download_image(url, filename, folder):
+    response = requests.get(url, allow_redirects=False)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    filepath = os.path.join(folder, filename)
+
+    with open(filepath, 'wb') as file:
+        file.write(response.content)
--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -3,83 +3,12 @@ import requests
 import os
 import json
 
-from bs4 import BeautifulSoup
+from parse_tululu import download_image
+from parse_tululu import download_text
+from parse_tululu import get_soup
+from parse_tululu import parse_book_page
 from urllib.parse import urljoin
-from urllib.parse import unquote
-from pathvalidate import sanitize_filename
 from pathlib import Path
-
-
-def check_for_redirect(response):
-    if response.status_code in (301, 302):
-        raise requests.HTTPError
-
-
-def get_soup(url):
-    response = requests.get(url, allow_redirects=False)
-    response.raise_for_status()
-    check_for_redirect(response)
-    return BeautifulSoup(response.text, 'lxml')
-
-
-def parse_book_page(soup, book_id):
-    title_selector = 'h1'
-    title_text = soup.select_one(title_selector).text
-    name, author = str(title_text).split('::')
-    stripped_name = name.strip()
-    stripped_author = author.strip()
-    book_filename = sanitize_filename(f'{book_id}. {stripped_name}.txt')
-
-    book = {
-        'Заголовок': stripped_name,
-        'Автор': stripped_author,
-        'book_filename': book_filename
-    }
-
-    genres_selector = 'span.d_book a'
-    genres_tag = soup.select(genres_selector)
-    book['Жанры'] = [genre.text for genre in genres_tag]
-
-    comments = soup.find_all('div', class_='texts')
-    book['Комментарии'] = [comment.find('span').text for comment in comments]
-
-    base_url = 'http://tululu.org'
-    img_selector = 'div.bookimage img'
-    img_tag = soup.select_one(img_selector)['src']
-    img_url = urljoin(base_url, img_tag)
-    book['img_url'] = img_url
-    book['img_filename'] = sanitize_filename(unquote(img_url.split('/')[-1]))
-
-    return book
-
-
-def download_text(filename, folder, book_id):
-    url = 'http://tululu.org/txt.php'
-    payload = {
-        'id': str(book_id)
-    }
-    response = requests.get(url, params=payload, allow_redirects=False)
-    response.raise_for_status()
-    check_for_redirect(response)
-
-    sanitazed_filename = sanitize_filename(filename)
-    Path(folder).mkdir(parents=True, exist_ok=True)
-    filepath = os.path.join(folder, sanitazed_filename)
-
-    with open(filepath, 'w') as file:
-        file.write(response.text)
-
-
-def download_image(url, filename, folder):
-    response = requests.get(url, allow_redirects=False)
-    response.raise_for_status()
-    check_for_redirect(response)
-
-    Path(folder).mkdir(parents=True, exist_ok=True)
-    filepath = os.path.join(folder, filename)
-
-    with open(filepath, 'wb') as file:
-        file.write(response.content)
 
 
 def create_parser():
@@ -105,6 +34,7 @@ def main():
     books_folder = os.path.join(dest_folder, 'books/')
     img_folder = os.path.join(dest_folder, 'pictures/')
     category_url = f'http://tululu.org/{args.category}/'
+    txt_base_url = 'http://tululu.org/txt.php'
 
     for page in (args.start_page, args.end_page+1):
         category_page_base_url = f'http://tululu.org/{args.category}/{page}'
@@ -121,7 +51,7 @@ def main():
                 books.append(book)
 
                 if not args.skip_txt:
-                    download_text(book['book_filename'], books_folder, book_id)
+                    download_text(txt_base_url, book['book_filename'], books_folder, book_id)
                 if not args.skip_imgs:
                     download_image(book['img_url'], book['img_filename'], img_folder)
             except requests.HTTPError:
