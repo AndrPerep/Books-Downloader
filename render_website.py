f7779290bf5a87e930a4da0b2f37e07602fe8eb5
AndrPerep <79010353+AndrPerep@users.noreply.github.com>
2022-04-13 18:53:17 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -6,18 +6,24 @@ from livereload import Server
 from more_itertools import chunked
 
 
-
 def on_reload():
-    template = env.get_template('template.html')
-    rendered_page = template.render(books=grouped_books)
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for page, page_books in enumerate(page_grouped_books):
+        col_grouped_books = list(chunked(page_books, 2))
+
+        template = env.get_template('template.html')
+        rendered_page = template.render(books=col_grouped_books)
+
+        page_file_name = os.path.join(pages_folder, f'index{page}.html')
+        with open(page_file_name, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
     print('Site rebuilt')
 
 
 if __name__ == '__main__':
     txt_folder = 'books/'
     img_folder = 'pictures/'
+    pages_folder = 'pages/'
+    os.makedirs(pages_folder, exist_ok=True)
 
     with open('books.json', 'r', encoding='utf-8') as books_file:
         books_json = books_file.read()
@@ -26,7 +32,8 @@ if __name__ == '__main__':
     for book in books:
         book['img_filepath'] = os.path.join(img_folder, book['img_filename'])
         book['txt_filepath'] = os.path.join(txt_folder, book['book_filename'])
-    grouped_books = list(chunked(books, 2))
+    page_grouped_books = list(chunked(books, 10))
+
 
     env = Environment(
         loader=FileSystemLoader('.'),
