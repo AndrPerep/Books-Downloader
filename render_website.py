6d7f77eaeac90d1bc6b135b1c0840160948451a3
AndrPerep <79010353+AndrPerep@users.noreply.github.com>
2022-04-16 16:53:35 +0300

    Add paginator

--- render_website.py
+++ render_website.py
@@ -3,18 +3,34 @@ import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
+from math import ceil
 from more_itertools import chunked
 
 
 def on_reload():
-    for page, page_books in enumerate(page_grouped_books):
-        col_grouped_books = list(chunked(page_books, 2))
+    page_grouped_books = list(chunked(books, books_on_page_num))
+    last_page = ceil(len(books)/books_on_page_num)
+    page_quantity = []
+    for page_number in range(1, last_page+1):
+        page_quantity.append(page_number)
 
+    for page_index, books_on_page in enumerate(page_grouped_books):
+        page = page_index+1
+        page_filename = f'index{page}.html'
+        page_path = os.path.join(pages_folder, page_filename)
+
+        col_grouped_books = list(chunked(books_on_page, 2))
         template = env.get_template('template.html')
-        rendered_page = template.render(books=col_grouped_books)
-
-        page_file_name = os.path.join(pages_folder, f'index{page}.html')
-        with open(page_file_name, 'w', encoding="utf8") as file:
+        rendered_page = template.render(
+            books=col_grouped_books,
+            current_page=page,
+            page_quantity=page_quantity,
+            page_filename=page_filename,
+            last_page=last_page,
+            prev_page=f'index{page-1}.html',
+            next_page=f'index{page+1}.html'
+        )
+        with open(page_path, 'w', encoding="utf8") as file:
             file.write(rendered_page)
     print('Site rebuilt')
 
@@ -23,6 +39,7 @@ if __name__ == '__main__':
     txt_folder = 'books/'
     img_folder = 'pictures/'
     pages_folder = 'pages/'
+    books_on_page_num = 10
     os.makedirs(pages_folder, exist_ok=True)
 
     with open('books.json', 'r', encoding='utf-8') as books_file:
@@ -30,10 +47,8 @@ if __name__ == '__main__':
     books = json.loads(books_json)
 
     for book in books:
-        book['img_filepath'] = os.path.join(img_folder, book['img_filename'])
-        book['txt_filepath'] = os.path.join(txt_folder, book['book_filename'])
-    page_grouped_books = list(chunked(books, 10))
-
+        book['img_filepath'] = os.path.join('../', img_folder, book['img_filename'])
+        book['txt_filepath'] = os.path.join('../', txt_folder, book['book_filename'])
 
     env = Environment(
         loader=FileSystemLoader('.'),
